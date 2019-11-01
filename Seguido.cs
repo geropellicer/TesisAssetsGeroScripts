223d7906fc3a1d5df18d45a0176df941d872ad86
Gero <geronimo@devsar.com>
2019-11-01 11:44:38 -0300

    cambio la funcion del random weighted index}

--- Seguido.cs
+++ Seguido.cs
@@ -130,29 +130,27 @@ public class Seguido : MonoBehaviour {
     /// <summary> Devuelve el indice al azar de uno de los pesos insertados en el array</summary>
     public int GetRandomWeightedIndex(int[] weights)
     {
-        // Get the total sum of all the weights.
-        int weightSum = 0f;
-        for (int i = 0; i < weights; ++i)
+        if(weights == null || weights.Length == 0) return -1;
+    
+        int t;
+        int i;
+        for(i = 0; i < weights.Length; i++)
         {
-            weightSum += weights[i];
+            if(weights[i] >= 0) w += weights[i];
         }
     
-        // Step through all the possibilities, one by one, checking to see if each one is selected.
-        int index = 0;
-        int lastIndex = elementCount - 1;
-        while (index < lastIndex)
-        {
-            // Do a probability check with a likelihood of weights[index] / weightSum.
-            if (Random.Range(0, weightSum) < weights[index])
-            {
-                return index;
-            }
+        float r = Random.value;
+        float s = 0f;
     
-            // Remove the last item from the sum of total untested weights and try again.
-            weightSum -= weights[index++];
+        for(i = 0; i < weights.Length; i++)
+        {
+            if (weights[i] <= 0f) continue;
+        
+            s += (float)weights[i] / total;
+            if (s >= r) return i;
         }
     
-        // No other item was selected, so return very last index.
-        return index;
+        return -1;
     }
+ 
 }
