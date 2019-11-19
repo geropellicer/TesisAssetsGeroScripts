7dbcf1bbb9443ae5ce2392c0290fbef2734d21f0
geropellicer <geropellicer@gmail.com>
2019-11-19 00:29:55 -0300

    Listo totems por OSC completo

--- OscAntenasController.cs
+++ OscAntenasController.cs
@@ -2,12 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
+Este archivo maneja la lógica de activar conn el entered nomas, pasandole la camara que corresponde. 
+Para los update de posiciones y demas ir al totem.cs
+
+*/
+
+
 public class OscAntenasController : MonoBehaviour
 {
+    public GameObject totem1, totem2, totem3, totem4;
+    OSC osc;
+    globalVariables gV;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        osc = GetComponent<OSC>();
+        osc.SetAddressHandler("/Cam1/Entered/", Cam1TotemEntered);
+        osc.SetAddressHandler("/Cam2/Entered/", Cam2TotemEntered);
+
+        osc.SetAddressHandler("/Cam1/Leave/", Cam1TotemLeave);
+        osc.SetAddressHandler("/Cam2/Leave/", Cam2TotemLeave);
+
+        osc.SetAddressHandler("/Cam1/Update/", Cam1TotemUpdate);
+        osc.SetAddressHandler("/Cam2/Update/", Cam2TotemUpdate);
+
+        osc.SetAddressHandler("/Antena1/Prendida/", PrenderAntena1);
+        osc.SetAddressHandler("/Antena2/Prendida/", PrenderAntena2);
+        osc.SetAddressHandler("/Antena3/Prendida/", PrenderAntena3);
+        osc.SetAddressHandler("/Antena4/Prendida/", PrenderAntena4);
+
+        osc.SetAddressHandler("/Antena1/Apagada/", ApagarAntena1);
+        osc.SetAddressHandler("/Antena2/Apagada/", ApagarAntena1);
+        osc.SetAddressHandler("/Antena3/Apagada/", ApagarAntena1);
+        osc.SetAddressHandler("/Antena4/Apagada/", ApagarAntena1);
+
+        gV = GameObject.Find("GlobalManager").GetComponent<globalVariables>();
     }
 
     // Update is called once per frame
@@ -15,4 +48,156 @@ public class OscAntenasController : MonoBehaviour
     {
         
     }
+
+    void Cam1TotemEntered(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().SetTrackeando(1);
+            totem1.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().SetTrackeando(1);
+            totem2.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().SetTrackeando(1);
+            totem3.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().SetTrackeando(1);
+            totem4.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        }
+    }
+    void Cam2TotemEntered(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().SetTrackeando(2);
+            totem1.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().SetTrackeando(2);
+            totem2.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().SetTrackeando(2);
+            totem3.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().SetTrackeando(2);
+            totem4.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        }
+    }
+
+
+    void Cam1TotemLeave(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().UnsetTrackeando(1);
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().UnsetTrackeando(1);
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().UnsetTrackeando(1);
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().UnsetTrackeando(1);
+        }
+    }
+    void Cam2TotemLeave(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().UnsetTrackeando(2);
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().UnsetTrackeando(2);
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().UnsetTrackeando(2);
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().UnsetTrackeando(2);
+        }
+    }
+
+    void Cam1TotemUpdate(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        }
+    }
+    void Cam2TotemUpdate(OscMessage m)
+    {
+        int idTotem = m.GetInt(0);
+        if(idTotem == 1)
+        {
+            totem1.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 2)
+        {
+            totem2.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 3)
+        {
+            totem3.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        } else if(idTotem == 4)
+        {
+            totem4.GetComponent<totem>().ActualizarTotemOsc(m.GetFloat(1), m.GetFloat(2), m.GetFloat(3));
+        }
+    }
+
+
+
+    void PrenderAntena1(OscMessage m)
+    {
+        totem1.GetComponent<totem>().EncenderTotem();
+    }
+    void PrenderAntena2(OscMessage m)
+    {
+        totem2.GetComponent<totem>().EncenderTotem();
+    }
+    void PrenderAntena3(OscMessage m)
+    {
+        totem3.GetComponent<totem>().EncenderTotem();
+    }
+    void PrenderAntena4(OscMessage m)
+    {
+        totem4.GetComponent<totem>().EncenderTotem();
+    }
+
+    void ApagarAntena1(OscMessage m)
+    {
+        totem1.GetComponent<totem>().ApagarTotem();
+    }
+    void ApagarAntena2(OscMessage m)
+    {
+        totem2.GetComponent<totem>().ApagarTotem();
+    }
+    void ApagarAntena3(OscMessage m)
+    {
+        totem3.GetComponent<totem>().ApagarTotem();
+    }
+    void ApagarAntena4(OscMessage m)
+    {
+        totem4.GetComponent<totem>().ApagarTotem();
+    }
+
+
 }
--- totem.cs
+++ totem.cs
@@ -14,16 +14,11 @@ public enum TIPOTOTEM
     PRIVADOENTRETENIMIENTO
 }
 
-[RequireComponent(typeof(AudioSource))]
+
 [RequireComponent(typeof(Animation))]
 public class totem : MonoBehaviour
 {
-    OSC osc;
     globalVariables gV;
-
-    /// <summary> La IP del arudino con el que se va a comunicar por OSC
-    /// Se debe asignar desde la lectura por webcam del patron (predefinir que patron es que tipo) </summary>
-    public string ipArduino;
 
     /// <summary> El tipo de totem que es este. En la escena debería haber uno de cada uno. 
     /// Se debe asignar desde la lectura por webcam del patron (predefinir que patron es que tipo) </summary>
@@ -41,9 +36,6 @@ public class totem : MonoBehaviour
 
     /// <summary> Referencia al SpriteRederer component </summary>
     SpriteRenderer im;
-    
-    /// <summary> Referencia a la fuente de sonido.!-- TOOD: deberiamos cambiar por el envio OSC al totem </summary>
-    AudioSource son;
 
     /// <summary> Referencia al animation component</summary>    
     Animation ani;
@@ -85,52 +77,17 @@ public class totem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        osc = GameObject.Find("OSC Manger").GetComponent<OSC>();
-
-        // Según que totem sea nos suscribimos al Tag OSC correspondiente
-        if(tipoEsteTotem == TIPOTOTEM.PRIVADOCOMERCIAL)
-        {
-            osc.SetAddressHandler("/Totem1/Entered/", EntroTotem);
-            osc.SetAddressHandler("/Totem1/Leave/", SalioTotem);
-            osc.SetAddressHandler("/Totem1/Update/", ActualizarTotemOsc);
-            osc.SetAddressHandler("/Totem1/Encender/", EncenderTotem);
-            osc.SetAddressHandler("/Totem1/Apagar/", ApagarTotem);
-        } else if(tipoEsteTotem == TIPOTOTEM.PRIVADOENTRETENIMIENTO)
-        {
-            osc.SetAddressHandler("/Totem2/Entered/", EntroTotem);
-            osc.SetAddressHandler("/Totem2/Leave/", SalioTotem);
-            osc.SetAddressHandler("/Totem2/Update/", ActualizarTotemOsc);
-            osc.SetAddressHandler("/Totem2/Encender/", EncenderTotem);
-            osc.SetAddressHandler("/Totem2/Apagar/", ApagarTotem);
-        } else if(tipoEsteTotem == TIPOTOTEM.PUBLICOESTATAL)
-        {
-            osc.SetAddressHandler("/Totem3/Entered/", EntroTotem);
-            osc.SetAddressHandler("/Totem3/Leave/", SalioTotem);
-            osc.SetAddressHandler("/Totem3/Update/", ActualizarTotemOsc);
-            osc.SetAddressHandler("/Totem3/Encender/", EncenderTotem);
-            osc.SetAddressHandler("/Totem3/Apagar/", ApagarTotem);
-        } else if(tipoEsteTotem == TIPOTOTEM.PUBLICOMILITAR)
-        {
-            osc.SetAddressHandler("/Totem4/Entered/", EntroTotem);
-            osc.SetAddressHandler("/Totem4/Leave/", SalioTotem);
-            osc.SetAddressHandler("/Totem4/Update/", ActualizarTotemOsc);
-            osc.SetAddressHandler("/Totem4/Encender/", EncenderTotem);
-            osc.SetAddressHandler("/Totem4/Apagar/", ApagarTotem);
-        }
 
         gV = GameObject.Find("GlobalManager").GetComponent<globalVariables>();
 
         im = GetComponent<SpriteRenderer>();
-        son = GetComponent<AudioSource>();
         ani = GetComponent<Animation>();
 
         ondas = transform.GetChild(0).gameObject;
 
         transmitiendo = false;
         trackeado = false;
-        if(!transmitiendo) {
-            ondas.SetActive(false);
-        }
+        ondas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -147,9 +104,6 @@ public class totem : MonoBehaviour
         if(trackeado)
         {
             transmitiendo = true;
-            //Prender audio
-            //Cambiar modo si es necesario
-            //Emitir señales visuales de transmision
             ondas.SetActive(true);
         }
     }
@@ -158,54 +112,43 @@ public class totem : MonoBehaviour
     void TerminarDeTransmitir()
     {
         transmitiendo = false;
-        im.enabled = false;
-        //Apagar audio
-        //Cambiar modo si es necesario
-        //Dejar de emitir señales visuales de transmision
         ondas.SetActive(false);
     }
 
-    /// <summary> Cuando el OSC detecta la imagen de este totem lo ponemos como trackeado </summary>
-    void SetTrackeando()
+    /// <summary> Cuando el OSC detecta la imagen de este totem lo ponemos como trackeado. 
+    /// Esta funcion se llama desde el OSCAntenasController</summary>
+    public void SetTrackeando(int cam)
     {
         trackeado = true;
         im.enabled = true;
-    }
-
-    /// <summary> Cuando lo perdemos por OSC lo destrackeamos y lo apagamos</summary>
-    void UnsetTrackeando() 
-    {
-        trackeado = false;
-        im.enabled = true;
-        transmitiendo = false;
-    }
-
-
-    void EntroTotem(OscMessage m)
-    {
-        SetTrackeando();
-        float numWebcam = m.GetInt(3);
-        if(numWebcam == 1)
+        if(cam == 1)
         {
             webcamTrackeando = WEBCAM.WEBCAM1;
-        } else if(numWebcam == 2)
+        } else if(cam == 2)
         {
             webcamTrackeando = WEBCAM.WEBCAM2;
         } else {
-            Debug.LogError("ERROR: El numero de kinect pasado por OSC no es invalido. Deberia ser 1 o 2. Se recibio: '" + numWebcam + "'");
+            Debug.LogWarning("ERROR: se paso el número de webcam ' " + cam + " ' cuando solo deberia ser 1 ó 2");
         }
-
+        
+        if(transmitiendo)
+        {
+            EmpezarATransmitir();
+        }
     }
 
-    void SalioTotem(OscMessage m)
+    /// <summary> Cuando lo perdemos por OSC lo destrackeamos y lo apagamos.
+     /// Esta funcion se llama desde el OSCAntenasController</summary>
+    public void UnsetTrackeando(int cam) 
     {
-        UnsetTrackeando();
+        trackeado = false;
+        im.enabled = false;
+
+        ondas.SetActive(false);
     }
 
-    void ActualizarTotemOsc(OscMessage m)
+    public void ActualizarTotemOsc(float x, float y, float rotZ)
     {
-        float x = m.GetFloat(0);
-        float y = m.GetFloat(1);
         if(webcamTrackeando == WEBCAM.WEBCAM1)
         {
             transform.localPosition = new Vector3(Utilidades.Map(x, minXin, maxXin, gV.minX, gV.maxX), Utilidades.Map(y, minYin, maxYin, gV.minY1, gV.maxY1), -0.1f);
@@ -213,43 +156,17 @@ public class totem : MonoBehaviour
         {
             transform.localPosition = new Vector3(Utilidades.Map(x, minXin, maxXin, gV.minX, gV.maxX), Utilidades.Map(y, minYin, maxYin, gV.minY2, gV.maxY2), -0.1f);
         }
+        transform.rotation = Quaternion.Euler(0,0, Utilidades.Map(rotZ, 0, 6, 0, 360));
+
     }
 
-    void EncenderTotem(OscMessage m)
+    public void EncenderTotem()
     {
         EmpezarATransmitir();
-        // Encender la luz del boton
-        // TODO: Forward port and IP
-        OscMessage message = new OscMessage();
-
-        message.address = "/EncenderTotem/Luz";
-        message.values.Add(1);
-        osc.Send(message);
-        
-        // Apagar el parlante
-        OscMessage message2 = new OscMessage();
-
-        message2.address = "/EncenderTotem/Sonido";
-        message2.values.Add(1);
-        osc.Send(message2);
     }
-    void ApagarTotem(OscMessage m)
+    public void ApagarTotem()
     {
         TerminarDeTransmitir();
-        // Apagar la luz del boton
-        // TODO: Forward port and IP
-        OscMessage message = new OscMessage();
-
-        message.address = "/ApagarTotem/Luz";
-        message.values.Add(0);
-        osc.Send(message);
-        
-        // Apagar el parlante
-        OscMessage message2 = new OscMessage();
-
-        message2.address = "/ApagarTotem/Sonido";
-        message2.values.Add(0);
-        osc.Send(message2);
     }
 
 
